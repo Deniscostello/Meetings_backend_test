e43f4dd58efdc8e8d043c3eb91b863638751173c
Denis Costello <G00396309@atu.ie>
2023-11-30 12:50:35 +0000

    First commit

---
+++ src/main/java/ie/atu/mettingtest/MettingTestApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.mettingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MettingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MettingTestApplication.class, args);
+    }
+
+}
